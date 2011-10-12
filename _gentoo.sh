87108ac792bd8c513cffc592a12e38b34046eb94
Dmitry Sigaev <www@sigaev.com>
2011-10-12 08:36:24 -0400

    Deal with ._cfg* files

--- _gentoo.sh
+++ _gentoo.sh
@@ -2,16 +2,28 @@ cd etc
 
 git reset origin/stage3
 git commit -amAuto-update
-git branch stage3
+git branch prev-stage3
 
-git rebase --onto {,origin/{,patch-}}stage3 || exit 1
+git rebase --onto {prev-,origin/{,patch-}}stage3 || exit 1
 git branch patch-stage3
+
+emerge -e world || exit 1
+
+git checkout -b {,prev-}stage3
+
+find -name ._cfg\* | while read e; do
+	mv "$e" "`sed s/^._cfg[0-9]*_// <<<$e`"
+done
+
+git commit -amAuto-update
+git rebase --onto {,prev-,patch-}stage3 || exit 1
+git branch -f patch-stage3
+git branch -d prev-stage3
 
 git rebase --onto {,origin/}patch-stage3 origin/emerged || exit 1
 git branch -f master
 git checkout -b emerged patch-stage3
 
-emerge -e world || exit 1
 emerge $(<.git/info/world) || exit 1
 
 git reset master

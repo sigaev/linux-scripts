3f25bb3d1e1f29b85e4e486bfcab47648617d733
Dmitry Sigaev <www@sigaev.com>
2011-10-19 22:13:13 -0400

    Install Windows fonts

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@ mount=/mnt
 stage3=http://mirror.mcs.anl.gov/pub/gentoo/releases/x86/current-stage3/stage3-i686-20111004.tar.bz2
 portage=http://mirror.mcs.anl.gov/pub/gentoo/snapshots/portage-latest.tar.xz
 git=http://sigaev.com/about/laptop/.git
-prg=http://sigaev.com/programs/cryptmount/.git
+prg=\
+http://sigaev.com/programs/cryptmount/.git \
+http://sigaev.com/programs/fonts-windows/.git \
 
 which git || sudo yum -y install git
 sudo mkfs.ext4 -m0 $disk

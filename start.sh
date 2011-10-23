3800dc7afd0702eecb244996837f8f066c91439c
Dmitry Sigaev <www@sigaev.com>
2011-10-22 23:58:15 -0400

    Update

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 disk=/dev/xvdf
 mount=/mnt
-stage3=http://mirror.mcs.anl.gov/pub/gentoo/releases/x86/current-stage3/stage3-i686-20111004.tar.bz2
+stage3=http://mirror.mcs.anl.gov/pub/gentoo/releases/x86/current-stage3/stage3-i686-20111018.tar.bz2
 portage=http://mirror.mcs.anl.gov/pub/gentoo/snapshots/portage-latest.tar.xz
 git=http://sigaev.com/about/laptop/.git
 prg=\

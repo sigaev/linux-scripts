7c8c925b1828dc55a1ecf2d07dc5db9bb42cb233
Dmitry Sigaev <www@sigaev.com>
2011-10-23 00:45:14 -0400

    Fix some more

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ http://sigaev.com/programs/cryptmount/.git
 http://sigaev.com/programs/fonts-windows/.git
 http://sigaev.com/programs/switch-root/.git
 "
+
 which git || sudo yum -y install git
 sudo mkfs.ext4 -m0 $disk
 sudo mount $disk $mount || exit 1
@@ -16,6 +17,6 @@ sudo mount $disk $mount || exit 1
 	cd /tmp
 	rm -f _install.sh
 	wget -q $git/scripts/_install.sh
-	sudo disk=$disk mount=$mount stage3=$stage3 portage=$portage git=$git prg=$prg \
+	sudo disk=$disk mount=$mount stage3=$stage3 portage=$portage git=$git prg="$prg" \
 		setsid nohup bash _install.sh </dev/null >/dev/null 2>&1 &
 )

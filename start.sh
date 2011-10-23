e44a69d76afe22a82817dec5f3ef0cf123cf52c9
Dmitry Sigaev <www@sigaev.com>
2011-10-23 00:34:49 -0400

    Fix bug

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@ mount=/mnt
 stage3=http://mirror.mcs.anl.gov/pub/gentoo/releases/x86/current-stage3/stage3-i686-20111018.tar.bz2
 portage=http://mirror.mcs.anl.gov/pub/gentoo/snapshots/portage-latest.tar.xz
 git=http://sigaev.com/about/laptop/.git
-prg=\
-http://sigaev.com/programs/cryptmount/.git \
-http://sigaev.com/programs/fonts-windows/.git \
-http://sigaev.com/programs/switch-root/.git \
-
+prg="
+http://sigaev.com/programs/cryptmount/.git
+http://sigaev.com/programs/fonts-windows/.git
+http://sigaev.com/programs/switch-root/.git
+"
 which git || sudo yum -y install git
 sudo mkfs.ext4 -m0 $disk
 sudo mount $disk $mount || exit 1

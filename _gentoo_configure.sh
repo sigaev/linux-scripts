fe1819ecd485553c193179d54c4c8559f35d9ce8
Dmitry Sigaev <www@sigaev.com>
2011-10-23 13:25:33 -0400

    Allow sigaev to set the root password

--- _gentoo_configure.sh
+++ _gentoo_configure.sh
@@ -5,7 +5,7 @@ echo 'sigaev:x:1000:100:Dmitry Sigaev:/home/sigaev:/bin/bash' >>passwd
 echo 'sigaev::10770:0:::::' >>shadow
 cat <<EOF >>sudoers
 Defaults:sigaev	env_keep=MC_FORMAT
-sigaev	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot
+sigaev	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot,/bin/passwd
 EOF
 for i in wheel audio video plugdev; do gpasswd -a sigaev $i; done
 (cd init.d; ln -sfn net.lo net.eth0)

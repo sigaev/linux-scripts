e2ba2d3fb2726aa314e000c958acd5a6ddaf01df
Dmitry Sigaev <www@sigaev.com>
2011-11-19 20:06:28 -0500

    Allow sigaev to run /sbin/shutdown, create /lib/modules symlink

--- _gentoo_configure.sh
+++ _gentoo_configure.sh
@@ -1,11 +1,12 @@
 cp ../usr/share/zoneinfo/America/New_York localtime
 ln -sfn /proc/self/mounts mtab
+ln -sfn ../boot ../lib/modules
 mkdir -m755 ../mnt/crypt
 echo 'sigaev:x:1000:100:Dmitry Sigaev:/home/sigaev:/bin/bash' >>passwd
 echo 'sigaev::10770:0:::::' >>shadow
 cat <<EOF >>sudoers
 Defaults:sigaev	env_keep=MC_FORMAT
-sigaev	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot,/bin/passwd
+sigaev	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot,/sbin/shutdown,/bin/passwd
 EOF
 mount -r /opt/VirtualBox/additions/VBoxGuestAdditions.iso /mnt
 /mnt/VBoxLinuxAdditions.run

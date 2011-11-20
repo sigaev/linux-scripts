503cd09b69ec49c0f72afca85f5b12a90dc4f139
Dmitry Sigaev <www@sigaev.com>
2011-11-20 18:47:27 -0500

    Add ubuntu:wheel

--- _gentoo_configure.sh
+++ _gentoo_configure.sh
@@ -2,8 +2,8 @@ cp ../usr/share/zoneinfo/America/New_York localtime
 ln -sfn /proc/self/mounts mtab
 ln -sfn ../boot ../lib/modules
 mkdir -m755 ../mnt/crypt
-echo "$user:x:1000:100:$name:/home/$user:/bin/bash" >>passwd
-echo "$user::10770:0:::::" >>shadow
+echo -e "ubuntu:x:6666:10::/:/bin/bash\n$user:x:1000:100:$name:/home/$user:/bin/bash" >>passwd
+echo -e "ubuntu:!:10770:0:::::\n$user::10770:0:::::" >>shadow
 cat <<EOF >>sudoers
 Defaults:$user	env_keep=MC_FORMAT
 $user	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot,/sbin/shutdown,/bin/passwd

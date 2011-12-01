9d442ba728058861988eb22680e6efc78ffee5a2
Dmitry Sigaev <www@sigaev.com>
2011-11-30 23:38:24 -0500

    Move prg= to config, fix uploading settings from the web

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 git=http://sigaev.com/programs/linux/root/.git
-prg="
-http://sigaev.com/programs/cryptmount/.git
-http://sigaev.com/programs/fonts-windows/.git
-http://sigaev.com/programs/switch-root/.git
-"
 
 wget -qO/dev/shm/config $git/scripts/config
 . /dev/shm/config
@@ -11,6 +6,6 @@ which git || sudo yum -y install git
 sudo mkfs.ext4 -m0 $disk
 sudo mount $disk $mount || exit 1
 wget -qO/dev/shm/_install.sh $git/scripts/_install.sh
-sudo disk=$disk mount=$mount stage3=$stage3 portage=$portage \
-	user=$user name="$name" git=$git prg="$prg" arch=$arch \
+sudo arch=$arch disk=$disk mount=$mount stage3=$stage3 portage=$portage \
+	user=$user name="$name" cfg=$cfg prg="$prg" git=$git \
 	setsid nohup bash /dev/shm/_install.sh </dev/null >/dev/null 2>&1 &

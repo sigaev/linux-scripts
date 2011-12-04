3e4e5233b936b114b2b75d15ec7a016b5630fb6b
Dmitry Sigaev <www@sigaev.com>
2011-12-04 16:00:29 -0500

    Support wireless

--- _gentoo_configure.sh
+++ _gentoo_configure.sh
@@ -1,6 +1,8 @@
 cp ../usr/share/zoneinfo/America/New_York localtime
 ln -sfn /proc/self/mounts mtab
+ln -sfn ../boot ../lib/firmware
 ln -sfn ../boot ../lib/modules
+ln -s ../../boot/secret/etc/wpa_supplicant/wpa_supplicant.conf wpa_supplicant/
 mkdir -m755 ../mnt/crypt
 echo -e "ubuntu:x:6666:10::/:/bin/bash\n$user:x:1000:100:$name:/home/$user:/bin/bash" >>passwd
 echo -e "ubuntu:!:10770:0:::::\n$user::10770:0:::::" >>shadow
@@ -8,26 +10,35 @@ cat <<EOF >>sudoers
 Defaults:$user	env_keep=MC_FORMAT
 $user	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot,/sbin/shutdown,/bin/passwd
 EOF
-cat <<EOF >local.d/90user.start
-#!/bin/bash
-if [[ ! -e /home/$user ]] && mkdir -m755 /home/$user; then
-	cd /home/$user
-	chown $user:users .
-	su -c '
-		for i in \`wget -qO- $cfg/Makefile | grep list= | cut -d= -f2\`; do
-			wget -qO- $cfg/\$i.txz | tar xJ
-		done
-	' - $user
-fi
+cat <<EOF >init.d/last
+#!/sbin/runscript
+
+depend() {
+	need net
+	use dns
+}
+
+start() {
+	if [[ ! -e /home/$user ]] && mkdir -m755 /home/$user; then
+		ebegin "Downloading settings from the web"
+		chown $user:users /home/$user
+		su -c '
+			wget -qO- $cfg/Makefile | grep list= | cut -d= -f2 | tr \\  \\\n \
+				| sed s,$,.txz, | wget -qO- -B$cfg/ -i- | tar xiJ
+		' - $user
+		rmdir /home/$user 2>/dev/null
+		eend \$((! \$?)) "Failed to download settings from the web"
+	fi
+}
 EOF
-chmod +x local.d/90user.start
+chmod +x init.d/last
 mount -r /opt/VirtualBox/additions/VBoxGuestAdditions.iso /mnt
 /mnt/VBoxLinuxAdditions.run
 umount /mnt
 mv X11/xorg.conf{,~}
 for i in wheel audio video cdrom plugdev vboxusers; do gpasswd -a $user $i; done
-(cd init.d; ln -sfn net.lo net.eth0)
-for i in dbus metalog acpid cryptmount cryptnmount ntpd net.eth0 sshd; do
+(cd init.d; ln -sfn net.lo net.eth0; ln -sfn net.lo net.wlan0)
+for i in dbus metalog acpid cryptmount cryptnmount last ntpd net.eth0 net.wlan0 sshd; do
 	rc-update add $i default
 done
 for i in dmcrypt consolefont alsasound; do

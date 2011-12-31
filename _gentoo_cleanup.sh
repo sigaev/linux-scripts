64564c45732fc8ed98564808aa999a26c92be85b
Dmitry Sigaev <www@sigaev.com>
2011-12-31 10:08:33 -0500

    Don't delete the remotes

--- _gentoo_cleanup.sh
+++ _gentoo_cleanup.sh
@@ -9,32 +9,14 @@ for i in . .git/scripts; do
 )
 done
 
-if [[ `git log -n1 --pretty=format:%H patch` != `git log -n1 --pretty=format:%H origin/patch` ]]; then
-(
-	mkdir ../tmp/a
-	cd ../tmp/a || exit 1
-	cp -a "$OLDPWD/.git" .
-	for i in . .git/scripts; do
-	(
-		cd $i
-		git remote set-head origin -d
-		git branch -r | xargs git branch -rd
-		git update-server-info
-		git status
-	)
-	done
-	git reset --hard patch
-	tar cJf ../a.txz --owner=$user --group=users .
-	cd .. && rm -fr a && chown $user:users a.txz
-)
-fi
 diff -u ../{etc/.git/scripts,var/lib/portage}/world 1>&2
 cat 1>&2 <<EOF
 Things that MUST be done:
 	* pay attention to the above diff (if any), consider updating $git/scripts/world
 	* examine the diff of /var/log/install.{out,err}.xz and previous versions
 	* same with /etc/{passwd,group}
+	* once it's clear the release is solid, update ${git%.git} with /etc/.git
 EOF
-[[ -e ../tmp/a.txz ]] && cat 1>&2 <<EOF
-	* update ${git%.git} with /tmp/a.txz
+[[ `git log -n1 --pretty=format:%H patch` != `git log -n1 --pretty=format:%H origin/patch` ]] && cat 1>&2 <<EOF
+	* WARNING /etc/.git changed!
 EOF

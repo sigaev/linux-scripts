b47dcb702395a98b1fa5eaf9854bc6e226314409
Dmitry Sigaev <www@sigaev.com>
2012-03-21 22:12:33 -0400

    Install pm-utils, remove ntpd from autostart, set hostname to $arch, add pm-suspend to sudo

--- _configure.sh
+++ _configure.sh
@@ -6,7 +6,10 @@ echo -e "ubuntu:x:6666:10::/:/bin/bash\n$user:x:1000:100:$name:/home/$user:/bin/
 echo -e "ubuntu:!:10770:0:::::\n$user::10770:0:::::" >>shadow
 cat <<EOF >>sudoers
 Defaults:$user	env_keep=MC_FORMAT
-$user	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot,/sbin/shutdown,/bin/passwd
+$user	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot,/sbin/shutdown,/bin/passwd,/usr/sbin/pm-suspend
+EOF
+cat <<EOF >conf.d/hostname
+hostname="$arch"
 EOF
 cat <<EOF >init.d/last
 #!/sbin/runscript
@@ -37,7 +40,7 @@ umount /mnt
 mv X11/xorg.conf{,~}
 for i in wheel audio video cdrom plugdev vboxusers; do gpasswd -a $user $i; done
 (cd init.d; ln -sfn net.lo net.eth0; ln -sfn net.lo net.wlan0)
-for i in dbus metalog acpid cryptmount cryptnmount last ntpd net.eth0 net.wlan0 allnet sshd; do
+for i in dbus metalog acpid cryptmount cryptnmount last net.eth0 net.wlan0 allnet sshd; do
 	rc-update add $i default
 done
 for i in dmcrypt consolefont alsasound; do

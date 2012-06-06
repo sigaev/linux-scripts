c8ce794e84012d48e52d46c9a85dc021948668cd
Dmitry Sigaev <www@sigaev.com>
2012-06-05 21:12:32 -0400

    Add xfsm-shutdown-helper to sudo

--- _configure.sh
+++ _configure.sh
@@ -6,7 +6,7 @@ echo -e "ubuntu:x:6666:10::/:/bin/bash\n$user:x:1000:100:$name:/home/$user:/bin/
 echo -e "ubuntu:!:10770:0:::::\n$user::10770:0:::::" >>shadow
 cat <<EOF >>sudoers
 Defaults:$user	env_keep=MC_FORMAT
-$user	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot,/sbin/shutdown,/bin/passwd,/usr/sbin/pm-suspend,/etc/init.d/cryptnmount,/sbin/btrfs
+$user	ALL=NOPASSWD:/usr/local/sbin/mount-crypt.sh,/usr/local/sbin/umount-crypt.sh,/sbin/reboot,/sbin/shutdown,/bin/passwd,/usr/sbin/pm-suspend,/etc/init.d/cryptnmount,/sbin/btrfs,/usr/lib/xfce4/session/xfsm-shutdown-helper
 EOF
 cat <<EOF >conf.d/hostname
 hostname="$arch"

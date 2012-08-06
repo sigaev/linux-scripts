84f31db19057a9ccf60bf4ed351a10d7eedf51a6
Dmitry Sigaev <www@sigaev.com>
2012-08-06 02:39:41 -0400

    Daemonize correctly

--- start.sh
+++ start.sh
@@ -11,5 +11,5 @@ wget -qO- $tgz_scripts | tar xzC ..
 sudo arch=$arch mount=$mount stage3=$stage3 tgz_portage=$tgz_portage \
 	user=$user name="$name" cfg=$cfg git_prg="$git_prg" \
 	git_root=$git_root git_root_ssh=$git_root_ssh \
-	setsid nohup bash ../*-linux-scripts-*/_install.sh \
-	</dev/null >/dev/null 2>&1 &
+	setsid nohup sh -c 'bash ../*-linux-scripts-*/_install.sh &' \
+	</dev/null >/dev/null 2>&1

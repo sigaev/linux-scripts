de7bc70c2b45851adb7e7fbfb9e28ee599f8d8bf
Dmitry Sigaev <www@sigaev.com>
2016-12-18 15:46:43 -0500

    Install unzip

--- arch/install.sh
+++ arch/install.sh
@@ -136,7 +136,7 @@ EOF
     pacman --noconfirm -Syu iw wpa_supplicant ntp alsa-utils base-devel vim \
                             xfce4 xorg-server kexec-tools git cpio wget \
                             xf86-input-libinput btrfs-progs graphviz xorg-xhost \
-                            squashfs-tools rsync noto-fonts-cjk tk unrar eog \
+                            squashfs-tools rsync noto-fonts-cjk tk unrar unzip eog \
                             evince libvdpau mplayer
     for i in linux; do
       pacman --noconfirm -Rs \$i --assume-installed \`pacman -Q \$i | tr \\  =\`

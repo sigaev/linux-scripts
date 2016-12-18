295e3e0fbfbf09d4262774e025f9fbfedc92a0a7
Dmitry Sigaev <www@sigaev.com>
2016-12-17 19:13:43 -0500

    Install NVidia drivers

--- arch/kernel.sh
+++ arch/kernel.sh
@@ -1,32 +1,33 @@
 # This installs Arch kernel into a newly created temporary directory.
 #
 # How to use:
-# 1. Update "nvidia" below.
-# 2. Run.
-# 3. Look in /tmp. There should be a new tmp.* directory there (let's call it $DIR).
-# 4. Copy $DIR to a new Btrfs snapshot.
-# 5. Add the new boot option. See install.sh for an example.
+# 1. Run.
+# 2. Look in /tmp. There should be a new tmp.* directory there (let's call it $DIR).
+# 3. Copy $DIR to a new Btrfs snapshot.
+# 4. Add the new boot option. See install.sh for an example.
 
 install_log=`mktemp`
 (
   set -x
-  nvidia=http://us.download.nvidia.com/XFree86/Linux-x86_64/375.26/NVIDIA-Linux-x86_64-375.26.run
 
   umask 022
   dir=`mktemp -d`
   mkdir $dir/{r,u,w}
   mount {-t,}overlay -olowerdir=/,upperdir=$dir/u,workdir=$dir/w $dir/r
-  rm -f $dir/r/usr/lib/firmware
-  systemd-nspawn -D$dir/r pacman --noconfirm -Syu linux
+  rm -fr $dir/r/{usr/lib/firmware,var/cache/pacman/pkg}
+  systemd-nspawn -D$dir/r --bind=/usr/lib/modules/pkg:/mnt bash -c \
+    'for i in /mnt/*; do tar xOJf $i .PKGINFO; done | grep ^pkgname | cut "-d " -f3 | xargs pacman --noconfirm -Rdd
+     pacman --noconfirm -Syu linux nvidia{,-libgl,-settings}'
   umount $dir/r
   (
     cd $dir
+    mkdir pkg
+    rm -f u/var/cache/pacman/pkg/nvidia-[0-9]*
     mv u/usr/lib/{firmware,modules/*} .
     mv u/boot/initramfs-linux-fallback.img initrd.lz
     mv u/boot/vmlinuz-linux vmlinuz.efi
+    mv u/var/cache/pacman/pkg/{libglvnd,libxnvctrl,nvidia}-* pkg/
     rm -fr r u w
-    mkdir pkg
-    curl -Lopkg/`basename $nvidia` $nvidia
   )
 
   echo $dir

d0240f692412764a47c159d00ee4cfa4261e88d8
Dmitry Sigaev <www@sigaev.com>
2017-02-19 01:06:44 -0500

    Install Python

--- arch/install.sh
+++ arch/install.sh
@@ -137,7 +137,7 @@ EOF
                             xfce4 xorg-server kexec-tools git cpio wget \
                             xf86-input-libinput btrfs-progs graphviz xorg-xhost \
                             squashfs-tools rsync noto-fonts-cjk tk unrar unzip eog \
-                            evince libvdpau mplayer
+                            evince libvdpau mplayer python
     for i in linux; do
       pacman --noconfirm -Rs \$i --assume-installed \`pacman -Q \$i | tr \\  =\`
     done

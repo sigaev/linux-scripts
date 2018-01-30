e5b54f8f1874fae94fd5e208e90f8e745f7f07f1
Dmitry Sigaev <www@sigaev.com>
2018-01-29 22:42:12 -0500

    Improve Google Drive scripts

--- gcloud/google-api-python-client.sh
+++ gcloud/google-api-python-client.sh
@@ -1,4 +1,8 @@
+cd
 sudo apt-get install python-setuptools
 curl https://pypi.python.org/packages/07/36/1304550b8a91e4f23258ae5880f56305528ce081cb00668107f02f153817/google-api-python-client-1.6.5.tar.gz | tar xvz
-cd google-api-python-client-1.6.5/
-sudo python setup.py install
+(
+	cd google-api-python-client-1.6.5/
+	sudo python setup.py install
+)
+python drive.py --noauth_local_webserver
